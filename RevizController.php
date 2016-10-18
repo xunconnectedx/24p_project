f2cca7fdce0dfcdd5fbcf732f802eac9447160af
xunconnectedx <divan.umnik@gmail.com>
2016-10-18 15:02:06 +0600

    приделал вариант через wget

--- RevizController.php
+++ RevizController.php
@@ -10,27 +10,87 @@ use yii\httpclient\Client;
 
 class RevizController extends Controller
 {
+    public static $rooturl='http://kristall-kino.ru';
     public function actionIndex(/*$url*/) //первая запись в базу (корень)
     {
-		$url = 'http://kristall-kino.ru';
+		//$url = 'http://kristall-kino.ru';
         $client = new Client();
         $response = $client->createRequest()
-        	->setUrl($url)->setOptions(['userAgent'=> 'Googlebot'])
+        	->setUrl(self::$rooturl)->setOptions(['userAgent'=> 'Googlebot'])
         	->send();
         if($response->isOk)
         {
         	$modelUrls = new Urls;	
         	$modelUrls->hash = $this->hash_test($response->content);
-			$modelUrls->url = $url;
+			$modelUrls->url = self::$rooturl;
 			$modelUrls->parsed = 0;
+            $modelUrls->ping = $this->pingsite(substr(self::$rooturl,7));//пинг
 			$modelUrls->save();
         }
         else echo "Проблема соединения!\n";
     }
-	//тестовое сообщения для гит
+
+    public function actionWgetDownload()
+    {
+        exec('wget -q -r -l 0 -p -U Googlebot -P site ' . 'http://kristall-kino.ru');
+    }
+
+    public function actionWgetRec()
+    {
+        $this->WgetRecord('site');
+    }
+
+    function WgetRecord($dir)
+    {
+        if (!is_dir($dir))
+        {
+            echo "Directory not found! \n";
+            return false;
+        } 
+        //$filemd5s = array();
+        $files = scandir($dir); //Список всех файлов в данной дерриктории
+        /*
+        Пробегаем по всем файлов в данной деррикотории
+        */
+        foreach ($files as $value)
+        {
+            //unset($filemd5s);
+            if (($value!= '.') && ($value != '..'))
+            {
+                //Если встретили папку, то окрываем еее
+                if (is_dir($dir.'/'.$value))
+                {
+                //echo "$dir$value \n";
+                $this->WgetRecord($dir.'/'.$value);
+                }
+                else
+                {   
+                    $val = $dir . "/" . $value;
+                    echo "$val\n" . hash_file('sha1', $val) . "\n\n";
+                    $modelUrls = new Urls;  
+                    $modelUrls->hash = hash_file('sha1', $val);
+                    $modelUrls->url = 'http://' . substr($val,5);
+                    $modelUrls->parsed = 0;
+                    $modelUrls->ping = $this->pingsite(substr(self::$rooturl,7));//пинг
+                    $modelUrls->save();      
+
+                }
+            }
+        }
+    }
+
+    function pingsite($url)
+    {
+        $ping = exec('ping -c2 ' . $url);
+        preg_match("/\/([0-9]*\.[0-9]*)\/[0-9]*\./", $ping, $outping);
+        if (empty($outping)) return '-';
+            else return $outping[1];
+
+    }
+
 	public function actionPars()
 	{
-		$rooturl='http://kristall-kino.ru';
+		//$rooturl='http://kristall-kino.ru';
 		global $countDown;
 		global $countObject;
 		$Pars = Urls::find()->where(['parsed'=> 0])->all(); //выбираем не спарсенные
@@ -60,7 +120,7 @@ class RevizController extends Controller
 
 	function Parsing($url)
 	{
-		$rooturl='http://kristall-kino.ru'; //href=\"\/user\/regis
+		//$rooturl='http://kristall-kino.ru'; //href=\"\/user\/regis
 		global $countDown;
 		global $countObject;
 		$client = new Client();
@@ -74,7 +134,7 @@ class RevizController extends Controller
         	//var_dump($out); //css js jpg png
 			foreach($out[1] as $value)
 			{
-				$url1 = $rooturl.$value; //забиваем найденную ссылку
+				$url1 = self::$rooturl.$value; //забиваем найденную ссылку
 				if (substr_count($value, '\/')) {continue;}
                 if ((substr_count($value, '.jpg')||substr_count($value, '.JPG')||substr_count($value, '.PNG')||substr_count($value, '.png')||substr_count($value, '.css')||substr_count($value, '.CSS')||substr_count($value, '.js')||substr_count($value, '.JS')||substr_count($value, '.swf')||substr_count($value, '.SWF')||substr_count($value, '.ico')||substr_count($value, '.ICO'))>0) 
                 	{
@@ -107,6 +167,7 @@ class RevizController extends Controller
             		$modelUrls = new Urls;	
             		$modelUrls->hash = $this->hash_test($url1);
 					$modelUrls->url = $url1;
+                    $modelUrls->ping = $this->pingsite($url1);//пинг
 					$modelUrls->save();
 			    } else echo "Проблема соединения";
 			}
@@ -118,5 +179,5 @@ class RevizController extends Controller
 		file_put_contents("File.html",$content);
 		return hash_file('sha1', 'File.html');
 		//unlink("File.html");
-	}
+	}   
 }    
